676f7e1dffe07f6fd6551426f9d6f737ccf614ed
LarianeArquero <arqueroloraine@gmail.com>
2025-02-19 21:19:13 +0800

    Add files via upload

---
+++ LA2_ARQUERO_CS1B/CS1B_LarianeAAquero_C1-A1.js
@@ -0,0 +1,39 @@
+/*NAME:LARIANE A. ARQUERO
+SUBJECT: DATA STRUCTURES AND ALGORITHM
+COURSE AND YEAR: BACHELOR OF SCIENCE IN COMPUTER SCIENCE
+INSTRUCTOR: CO Pascua
+ACTIVITY No. LA2_ARQUERO_CS1B
+assignment1.js*/
+
+//This is my personal information
+let fullname1 = 'Lariane Ancheta Arquero'; //declared my full name    
+let birthdate1 = 'November 19, 2005'; //declared my birthdate
+let birthplace1 = 'Baluarte, Salcedo, Ilocos Sur, 2711'; //declared my birthplace
+let address1 = 'Baluarte, Salcedo, Ilocos Sur,2711'; //declared my currently address
+let courseAndYear1 = 'Bachelor of Science in Computer Science'; //declared my course
+let dreamJob1 = 'Software Engineer'; //declred my dreams that we want to achieve
+
+console.log(`[${fullname1.toUpperCase()}] is born on [${birthdate1}], at [${birthplace1.toUpperCase()}], and currently living in [${address1.toUpperCase()}]`); 
+console.log(`[${fullname1.toLowerCase()}] is taking up [${courseAndYear1}] ,and dream to be a [${dreamJob1}] after graduation.`)
+
+//This is the first personal information about my classmate
+let fullname2 = 'Princess Eulla Pedrozo Haduca'; //declared the full name of my first classmate
+let birthdate2 = 'July 12, 2006'; //declared the birthdate of my first classmate
+let birthplace2 = 'Damacuag, Santa Lucia, Ilocos Sur,2712'; //declared the birthplace of my first classmates
+let address2 = 'Damacuag, Santa Lucia, Ilocos Sur, 2712'; //declared the address of my first classmate
+let courseAndYear2 ='Bachelor of Science in Computer Science'; //declared the course of my first classmate
+let dreamJob2 = 'Software Engineer'; //declared the dream job of my classmate that she wants to achieve
+
+console.log(`[${fullname2.toUpperCase()}] is born on [${birthdate2}], at [${birthplace2.toUpperCase()}], and currently living in [${address2.toUpperCase()}]`); 
+console.log(`[${fullname2.toLowerCase()}] is taking up [${courseAndYear2}] ,and dream to be a [${dreamJob2}] after graduation.`)
+
+//This is the second personal information about my classmate
+let fullname3 = 'R Jay Luis Cacalda Acosta'; //declared the full name of my second classmate
+let birthdate3 = 'May 4, 2005'; //declared the birthdate of my second classmate
+let birthplace3 = 'Santa Lucia, District Hospital'; //declared the birthplace of my second classmate 
+let address3 = 'Ayusan, Santa Lucia, Ilocos Sur'; //declared the currently address of my second classmate
+let courseAndYear3 = 'Bachelor of Science in Computer Science'; //declared the course of my second classmate
+let dreamJob3 = 'Cyber Security'; //declared the job of my classmate he wants to achieve
+    
+console.log(`[${fullname3.toUpperCase()}] is born on [${birthdate3}], at [${birthplace3.toUpperCase()}], and currently living in [${address2.toUpperCase()}]`); 
+console.log(`[${fullname3.toLowerCase()}] is taking up [${courseAndYear3}] ,and dream to be a [${dreamJob3}] after graduation.`)
