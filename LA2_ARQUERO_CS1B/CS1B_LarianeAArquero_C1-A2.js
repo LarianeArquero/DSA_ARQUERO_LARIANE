604f7821cb18fde2d3a711566ac7c5835e67e18f
LarianeArquero <arqueroloraine@gmail.com>
2025-02-20 10:53:05 +0800

    Add files via upload

--- LA2_ARQUERO_CS1B/CS1B_LarianeAArquero_C1-A2.js
+++ LA2_ARQUERO_CS1B/CS1B_LarianeAArquero_C1-A2.js
@@ -6,26 +6,25 @@ ACTIVITY No. LA2_ARQUERO_CS1B
 assignment2.js*/
 
 //initialize the 4 variables and 1 constant
-let a = '25'; //this is integer 
-let b = '20'; //this is integer
-let c = '12'; //this is integer
+let a = 25; //this is integer 
+let b = 20; //this is integer
+let c = 12; //this is integer
 let d = "15"; //this is string because it is enclosed in double quotation mark.
-const e = '15'; //this is constant
+const e = 15; //this is constant
 
-let sum = a + b + c + (d) + e;
-console.log(sum)
+console.log(a + b + c + Number(d) + e);
 
 // compare the variables of d and e
-console.log(d > e)//It's d is greater than e
-console.log(d < e)//It's d is lesser than e
-console.log(d >= e)//It's d is greater than or equal to e
-console.log(d <= e)//It's d is lesser than or equal to e
-console.log(d === e)//It's d is strict equality to e
+console.log(Number(d) > e)//It's d is greater than e
+console.log(Number(d) < e)//It's d is lesser than e
+console.log(Number(d) >= e)//It's d is greater than or equal to e
+console.log(Number(d) <= e)//It's d is lesser than or equal to e
+console.log(Number(d)=== e)//It's d is strict equality to e
 
 //subtract all the variables that we declared
-let subraction = a - c - (d) - e;
+let subraction = (a - b - c - Number(d) - e);
 console.log(subraction)
-
+ 
 //multiply the values of variable a and c then divide to constant e
 let multiplyAndDivide = (a * c) /e;
 console.log(multiplyAndDivide)
