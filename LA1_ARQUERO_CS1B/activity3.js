3e5737ec7e3f055fcb63977d9cc4ee5e4db3d80a
LarianeArquero <arqueroloraine@gmail.com>
2025-02-26 15:52:51 +0800

    Add files via upload

---
+++ LA1_ARQUERO_CS1B/activity3.js
@@ -0,0 +1,15 @@
+/*Name: LARIANE A. ARQUERO
+Course&Section: BSCS 1B
+Activity No. LA1
+*/
+
+//Boolean Activity
+let isJsProgrammingLanguage = true; 
+let isJsHard = false; 
+let favNumb = 19; 
+favNumb = undefined; 
+
+//Console.log the results
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb);
---
+++ LA1_ARQUERO_CS1B/activity5.js
@@ -0,0 +1,19 @@
+/*Name: LARIANE A. ARQUERO
+Course&Section: BSCS 1B
+Activity No. LA1
+*/
+
+//Strings
+let favActorFirstName = "Jackie"; 
+let favActorLastName = "Chan"; 
+let fullName = favActorFirstName +" "+ favActorLastName; 
+let uppercase = fullName.toUpperCase(); 
+let message = `My Favorite Actor is ${uppercase},`; 
+message = `he is the most and influential Chinese actors`; 
+
+//Console.log the results
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
