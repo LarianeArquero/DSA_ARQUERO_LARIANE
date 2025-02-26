c7e6a287b1ac22dd20b327905da8147ff6e391c7
LarianeArquero <arqueroloraine@gmail.com>
2025-02-26 22:26:49 +0800

    Add files via upload

---
+++ LA1_ARQUERO_CS1B/boolean.js
@@ -0,0 +1,14 @@
+//Boolean Activity
+let isJsProgrammingLanguage = true; 
+//Let's create a variable isJSProgrammingLanguage the value is true
+let isJsHard = false; 
+//Let's create a variable isJSHard the value is false
+let favNumb = 19; 
+//Let's create a variable is my favorite number is 19
+favNumb = undefined; 
+//The favorite number is undifined
+
+//Console.log the results
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb);
---
+++ LA1_ARQUERO_CS1B/number.js
@@ -0,0 +1,23 @@
+//Numbers Activity
+//Let's create variables for my first favorite number
+let firstFavNum = 19; 
+//Let's create variables for my second favorite number
+let secondFavNum = 5; 
+
+//Arithmetic Operations
+console.log(firstFavNum + secondFavNum); 
+//let's add my first favorite number and my second favorite number is 24.
+console.log(firstFavNum - secondFavNum); 
+//let's subtract my first favorite number and my second favorite number is 14.
+console.log(firstFavNum * secondFavNum); 
+//let's multiply my first favorite number and my second favorite number is 95.
+console.log(firstFavNum / secondFavNum); 
+//let's divide my first favorite number and my second favorite number is 3.8.
+console.log(firstFavNum % secondFavNum); 
+//let's mod my first favorite number and my second favorite number is 4.
+console.log(firstFavNum ** secondFavNum); 
+//let's exponentiation my first favorite number and my second favorite number is 2476099.
+
+
+
+
