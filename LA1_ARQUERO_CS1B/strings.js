0369ddd866b97a05f07be4268ca913ecab6da546
LarianeArquero <arqueroloraine@gmail.com>
2025-02-26 22:20:10 +0800

    Add files via upload

---
+++ LA1_ARQUERO_CS1B/strings.js
@@ -0,0 +1,26 @@
+/*Name: LARIANE A. ARQUERO
+Course&Section: BSCS 1B
+Activity No. LA1
+*/
+
+//Strings
+//My favorite actor is Jackie Chan
+let favActorFirstName = "Jackie"; 
+//Let's create a variable named of my favorite actor first name
+let favActorLastName = "Chan"; 
+//Let's create a variable named of my favorite actor last name
+let fullName = favActorFirstName +" "+ favActorLastName; 
+//Let's create a variable named full name then let add the first name and second name
+let uppercase = fullName.toUpperCase(); 
+//Let's create a variable named to uppercase the fullname
+let message = `My Favorite Actor is [${uppercase}],`; 
+//Let's create a variable named my favorite actor
+message += `the most and influential Chinese actors.`; 
+//Let's append the message
+
+//Console.log the results
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
